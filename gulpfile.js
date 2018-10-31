2a2b3be0bcf989184a37b09e06ca6564247887af
capnmidnight <sean.mcbeth+gh@gmail.com>
2018-10-30 23:41:07 -0400

    update build tool and add UMD MAVERIC conf to talks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,32 @@
-require("marigold-build").defaultSetup({
-  skipJavaScript: true,
-  express: require("./src/server/express")
-});
+var gulp = require("gulp"),
+  pkg = require("./package.json"),
+  marigold = require("marigold").setup(gulp, pkg),
+
+  css = marigold.css(["*.styl"]),
+
+  html = marigold.html(["*.pug"]),
+
+  images = marigold.images(["*.png", "*.jpg", "*.gif", "*.svg", "img/*.png", "img/*.jpg", "img/*.svg"]),
+
+  devServer = marigold.devServer([
+      "*.pug",
+      "*.styl"
+    ], [
+      "!gulpfile.js",
+      "*.css",
+      "*.html"
+    ], {
+      path: ".",
+      url: ""
+    });
+
+marigold.taskify(
+  [
+    html,
+    css,
+    images
+  ], {
+    default: devServer
+  });
+
+gulp.task("test", gulp.series(["release"]), devServer);
