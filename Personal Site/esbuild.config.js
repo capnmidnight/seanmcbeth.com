3ae7a354ebc6b48cb9e3a2b885bd124e45241035
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-06-03 17:09:51 -0400

    auto-build apps

--- Personal Site/esbuild.config.js
+++ Personal Site/esbuild.config.js
@@ -1,11 +1,23 @@
 import { Build } from "@juniper-lib/esbuild";
 import { glsl } from "esbuild-plugin-glsl";
+import { promises } from "fs";
 
-await new Build(process.argv.slice(2))
+const { readdir } = promises;
+
+const dirs = await readdir("./src", {
+    withFileTypes: true
+});
+
+const apps = dirs.filter(dir => dir.isDirectory() && dir.name.endsWith("-app"))
+    .map(dir => dir.name);
+
+const build = new Build(process.argv.slice(2))
     .plugin((minify) => glsl({ minify }))
-    .external("three")
+    .external("three");
 
-    .bundle("junk-app")
-    .bundle("yabs-app")
+for (const app of apps) {
+    console.log(build.buildType, app);
+    build.bundle(app);
+}
 
-    .run();
+await build.run();
