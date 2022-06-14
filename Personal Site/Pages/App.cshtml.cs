88f773950299d80f06fb4bbd678864194ccad7a4
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-06-13 22:08:41 -0400

    only load Three.js if a file asks for it

--- Personal Site/Pages/App.cshtml.cs
+++ Personal Site/Pages/App.cshtml.cs
@@ -5,9 +5,11 @@ namespace SeanMcBeth.Pages
 {
     public class AppModel : PageModel
     {
+        private static readonly DirectoryInfo appRoot = 
+            new DirectoryInfo("wwwroot").CD("js");
+
         public static readonly string[] AppNames =
-            new DirectoryInfo("wwwroot")
-                .CD("js")
+            appRoot
                 .EnumerateDirectories()
                 .Where(d => d.Name.EndsWith("-app")
                     && d.Name.ToLowerInvariant() != "junk-app"
@@ -18,16 +20,38 @@ namespace SeanMcBeth.Pages
         [BindProperty(SupportsGet = true), FromRoute]
         public string? Name { get; set; }
 
+        public bool IncludeThreeJS { get; private set; }
+
         public string ScriptPath => string.Join('/', "js", Name + "-app", "index.js");
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
             if (!AppNames.Contains(Name))
             {
                 return NotFound();
             }
 
+            IncludeThreeJS = await HasThreeJS();
             return Page();
+        }
+
+        private async Task<bool> HasThreeJS()
+        {
+            using var reader = appRoot
+                .CD(Name + "-app")
+                .Touch("index.js")
+                .OpenText();
+
+            while (!reader.EndOfStream)
+            {
+                var line = await reader.ReadLineAsync();
+                if (line?.Contains("THREE.") == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
