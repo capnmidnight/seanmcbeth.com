1a6df69cd9fed4b9ae922ed7ca9caede62b7cb76
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-07-25 21:50:55 -0400

    button demo

---
+++ Personal Site/src/button-app/index.ts
@@ -0,0 +1,37 @@
+﻿import { objGraph } from "@juniper-lib/threejs/objects";
+import { TextMeshButton } from "@juniper-lib/threejs/widgets/TextMeshButton";
+import { TextImageOptions } from "@juniper-lib/graphics2d/TextImage";
+import { createTestEnvironment } from "../createTestEnvironment";
+
+const buttonStyle: Partial<TextImageOptions> = {
+    bgFillColor: "blue",
+    bgStrokeColor: "white",
+    bgStrokeSize: 0.01,
+    scale: 500,
+    padding: 0.05,
+    maxHeight: 0.25,
+    minHeight: 0.25,
+    wrapWords: false
+};
+
+(async function () {
+    const env = await createTestEnvironment();
+
+    await env.fadeOut();
+    await env.load();
+
+    const button1 = new TextMeshButton(env, "Button1", "Click Me?", buttonStyle);
+    button1.enabled = false;
+    button1.addEventListener("click", () => env.audio.playClip("footsteps"));
+    button1.object.position.set(-0.5, 1.75, -3);
+
+    const button2 = new TextMeshButton(env, "Button2", "Click Me.", buttonStyle);
+    button2.addEventListener("click", () => button1.enabled = !button1.enabled);
+    button2.object.position.set(0.5, 1.75, -3);
+
+    objGraph(env.foreground,
+        button1,
+        button2);
+    
+    await env.fadeIn();
+})();
