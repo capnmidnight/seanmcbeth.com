c0684d9a3a1bfe0552bb11e7c146465086e5b9ec
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-07-26 15:43:00 -0400

    set execute bit for site

--- etc/rebuild.sh
+++ etc/rebuild.sh
@@ -4,5 +4,6 @@ mv ~/bin/SeanMcBeth.Site/certs ~/bin/SeanMcBeth.Site.Certs
 rm -rf ~/bin/SeanMcBeth.Site/*
 cp -r ~/src/seanmcbeth.com/deploy/linux/* ~/bin/SeanMcBeth.Site
 mv ~/bin/SeanMcBeth.Site.Certs ~/bin/SeanMcBeth.Site/certs
+chmod 700 ~/bin/SeanMcBeth.Site/SeanMcBeth.Site
 
 sudo systemctl start SeanMcBeth.Site
