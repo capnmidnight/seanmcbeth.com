7e58cbe0f4984ed7f0b286a4d5233833a8db95c3
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-07-26 09:58:16 -0400

    v1.2.3

--- Personal Site/wwwroot/js/offscreen-canvas-app/index.js
+++ Personal Site/wwwroot/js/offscreen-canvas-app/index.js
@@ -1529,6 +1529,45 @@ function BackgroundVideo(autoplay, mute, looping, ...rest) {
   return Video(playsInline(true), controls(false), muted(mute), autoPlay(autoplay), loop(looping), styles(display("none")), ...rest);
 }
 
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/threejs/animation/scaleOnHover.ts
+var scaledItems = singleton("Juniper:ScaledItems", () => /* @__PURE__ */ new Map());
+function removeScaledObj(obj) {
+  const state = scaledItems.get(obj);
+  if (state) {
+    scaledItems.delete(obj);
+    state.dispose();
+  }
+}
+
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/threejs/cleanup.ts
+function cleanup(obj) {
+  const cleanupQ = new Array();
+  const cleanupSeen = /* @__PURE__ */ new Set();
+  cleanupQ.push(obj);
+  while (cleanupQ.length > 0) {
+    const here = cleanupQ.shift();
+    if (here && !cleanupSeen.has(here)) {
+      cleanupSeen.add(here);
+      if (here.isMesh) {
+        cleanupQ.push(here.material, here.geometry);
+      }
+      if (here.isMaterial) {
+        cleanupQ.push(...Object.values(here));
+      }
+      if (here.isObject3D) {
+        cleanupQ.push(...here.children);
+        here.clear();
+        removeScaledObj(here);
+      }
+      if (isArray(here)) {
+        cleanupQ.push(...here);
+      }
+      dispose(here);
+    }
+  }
+  cleanupSeen.clear();
+}
+
 // ../Juniper/src/Juniper.TypeScript/@juniper-lib/dom/canvas.ts
 var hasHTMLCanvas = "HTMLCanvasElement" in globalThis;
 var hasHTMLImage = "HTMLImageElement" in globalThis;
@@ -3209,15 +3248,17 @@ function rect(g, color, x, y) {
 }
 
 addEventListener("message", (evt) => {
-  if (evt.data instanceof OffscreenCanvas) {
-    const g = evt.data.getContext("2d");
+  if (evt.data instanceof Array) {
+    const canv = new OffscreenCanvas(evt.data[0], evt.data[1]);
+    const g = canv.getContext("2d");
     const drawRect = (i2, x, y) => rect(g, colors[i2 % colors.length], x, y);
     const draw = (i2) => {
       drawRect(i2, 0, 0);
       drawRect(i2 + 1, 0, 1);
       drawRect(i2 + 2, 1, 0);
       drawRect(i2 + 3, 1, 1);
-      postMessage("update");
+      const img = canv.transferToImageBitmap();
+      postMessage(img, [img]);
     };
     let i = 0;
     draw(i);
@@ -3235,41 +3276,43 @@ var workerScriptBlobUrl = URL.createObjectURL(workerScriptBlob);
   const canvas1 = document.createElement("canvas");
   canvas1.width = 100;
   canvas1.height = 100;
-  const map2 = makeMesh(env, canvas1, -1);
-  const g = canvas1.getContext("2d");
-  const drawRect = (i2, x, y) => rect(g, colors[i2 % colors.length], x, y);
+  const map1 = makeMesh(env, canvas1, -1);
+  const g1 = canvas1.getContext("2d");
+  const drawRect = (i2, x, y) => rect(g1, colors[i2 % colors.length], x, y);
   const draw = (i2) => {
     drawRect(i2, 0, 0);
     drawRect(i2 + 1, 0, 1);
     drawRect(i2 + 2, 1, 0);
     drawRect(i2 + 3, 1, 1);
-    postMessage("update");
   };
   let i = 0;
   draw(i);
   setInterval(() => {
     draw(++i);
-    map2.needsUpdate = true;
+    map1.needsUpdate = true;
   }, 1e3);
-  const canvas2 = document.createElement("canvas");
-  canvas2.width = 100;
-  canvas2.height = 100;
-  const map1 = makeMesh(env, canvas2, 1);
+  const map2 = makeMesh(env, null, 1);
   const worker = new Worker(workerScriptBlobUrl);
-  const offscreen = canvas2.transferControlToOffscreen();
-  worker.postMessage(offscreen, [offscreen]);
+  worker.postMessage([100, 100]);
   worker.addEventListener("message", (evt) => {
-    if (evt.data === "update") {
-      map1.needsUpdate = true;
+    if (evt.data instanceof ImageBitmap) {
+      cleanup(map2.image);
+      map2.image = evt.data;
+      map2.needsUpdate = true;
     }
   });
   await env.fadeIn();
 })();
+var geom = new THREE.PlaneBufferGeometry(1, 1, 1, 1);
+var uv = geom.getAttribute("uv");
+for (let i = 0; i < uv.count; ++i) {
+  uv.setY(i, 1 - uv.getY(i));
+}
 function makeMesh(env, canvas1, x) {
-  const geom = new THREE.PlaneBufferGeometry(1, 1, 1, 1);
   const map = new THREE.CanvasTexture(canvas1);
   const mat = new THREE.MeshBasicMaterial({ map });
   const mesh = new THREE.Mesh(geom, mat);
+  map.flipY = false;
   env.foreground.add(mesh);
   mesh.position.set(x, 1.75, -3);
   return map;
