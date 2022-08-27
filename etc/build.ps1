1e3a89d7b7e9ee07035cbbd07f746395cad36031
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-08-26 20:29:05 -0400

    move stuff around

--- etc/build.ps1
+++ etc/build.ps1
@@ -1,10 +1,12 @@
 cd ..\
     # Bundle the TypeScript code
     dotnet run --project ".\Build\"
-
-    # Create the .NET package
-    dotnet publish ".\Personal Site\" --configuration Release --no-self-contained --runtime linux-x64 --output ..\deploy\linux\
-
-    # Archive the .NET package for deployment
-    7z a -ttar ..\deploy\seanmcbeth.com.linux.tar ..\deploy\linux\*
 cd -
+    
+mkdir ..\deploy\
+
+# Create the .NET package
+dotnet publish "..\Personal Site\" --configuration Release --no-self-contained --runtime linux-x64 --output ..\deploy\linux\
+
+# Archive the .NET package for deployment
+7z a -ttar ..\deploy\seanmcbeth.com.linux.tar ..\deploy\linux\*
