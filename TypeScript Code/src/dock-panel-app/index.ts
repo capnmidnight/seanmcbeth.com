aa24992526c0645d5aa41f36df41dfe3e535da58
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-08-27 20:18:49 -0400

    fixup rearrangeable

--- TypeScript Code/src/dock-panel-app/index.ts
+++ TypeScript Code/src/dock-panel-app/index.ts
@@ -1,14 +1,18 @@
-import { height, width } from "@juniper-lib/dom/css";
+import { gap, height, width } from "@juniper-lib/dom/css";
 import { elementApply, H3 } from "@juniper-lib/dom/tags";
-import { DockCell, DockCol, DockPanel, DockRow } from "@juniper-lib/widgets/DockPanel";
+import { DockCell, DockColumn, DockPanel, DockRow } from "@juniper-lib/widgets/DockPanel";
 
 
 elementApply(document.body.querySelector("main"),
     DockPanel(
+        "Test",
+        "resizable",
+        "rearrangeable",
         width("100%"),
         height("90vh"),
+        gap("3px"),
         DockRow(
-            DockCol(
+            DockColumn(
                 DockCell(
                     H3("C1"),
                     "Lorem ipsum"
