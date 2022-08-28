fb9f7d2627b15794457c76ba0b0c8d8a7d3adc12
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-08-28 14:26:45 -0400

    better dock panel

--- TypeScript Code/src/dock-panel-app/index.ts
+++ TypeScript Code/src/dock-panel-app/index.ts
@@ -1,18 +1,32 @@
-import { gap, height, width } from "@juniper-lib/dom/css";
-import { elementApply, H3 } from "@juniper-lib/dom/tags";
-import { DockCell, DockColumn, DockPanel, DockRow } from "@juniper-lib/widgets/DockPanel";
+import { backgroundColor, border, borderRadius, gridGap, height, rule, width } from "@juniper-lib/dom/css";
+import { elementApply, H3, Style } from "@juniper-lib/dom/tags";
+import { DockCell, DockGroupColumn, DockPanel, DockGroupRow, rearrangeable, resizable } from "@juniper-lib/widgets/DockPanel";
 
+Style(
+    rule(".dock.panel",
+        width("100%"),
+        height("90vh")
+    ),
+
+    rule(".dock.cell",
+        border("1px solid black"),
+        borderRadius("5px"),
+        backgroundColor("#ccc")
+    ),
+
+    rule(".dock.cell > .content",
+        backgroundColor("#fff")
+    )
+);
 
 elementApply(document.body.querySelector("main"),
     DockPanel(
         "Test",
-        "resizable",
-        "rearrangeable",
-        width("100%"),
-        height("90vh"),
-        gap("3px"),
-        DockRow(
-            DockColumn(
+        resizable(true),
+        rearrangeable(true),
+        gridGap("3px"),
+        DockGroupRow(
+            DockGroupColumn(
                 DockCell(
                     H3("C1"),
                     "Lorem ipsum"
