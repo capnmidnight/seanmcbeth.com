265fa41ccb9659d1dc02f13ac9a7f985c03d2bac
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-09-01 15:18:27 -0400

    tree view auto scrolling demo

--- TypeScript Code/src/tree-view-app/index.ts
+++ TypeScript Code/src/tree-view-app/index.ts
@@ -1,4 +1,4 @@
-import { height } from "@juniper-lib/dom/css";
+import { height, left, position, top, width } from "@juniper-lib/dom/css";
 import { elementApply } from "@juniper-lib/dom/tags";
 import { TreeView } from "@juniper-lib/widgets/TreeView";
 
@@ -8,15 +8,27 @@ for (let i = 0; i < 0xff; ++i) {
 }
 
 const tv = new TreeView<number>({
+    defaultLabel: "numbers",
     getParent: v => v === 0 ? null : v >> 1,
     canHaveChildren: v => parseFloat(v.toString()) === v,
     getChildDescription: v => v.toString(),
     getDescription: v => v.toLocaleString(),
-    getLabel: v => v.toFixed(3),
+    getLabel: v => v.toFixed(0),
     getOrder: v => parseFloat(v.toString())
 },
-    height("100vh"));
+    height("100vh"),
+    width("100vw"),
+    position("absolute"),
+    top(0),
+    left(0)
+);
 
 elementApply(document.body, tv);
 
 tv.values = values;
+
+let counter = 0;
+setInterval(() => {
+    counter = (counter + 0x7e) % 0xff;
+    tv.selectedValue = counter;
+}, 1000);
