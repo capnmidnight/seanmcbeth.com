849863b95579b99788fc75d67cab189cd3878de2
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-09-01 13:29:45 -0400

    content updates

---
+++ Personal Site/Models/MenuItem.cs
@@ -0,0 +1,4 @@
+﻿namespace SeanMcBeth.Models
+{
+    public record MenuItem(string Text, string Label, string Target, bool Hide = false);
+}
--- Personal Site/Pages/Index.cshtml.cs
+++ Personal Site/Pages/Index.cshtml.cs
@@ -4,18 +4,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SeanMcBeth.Pages
 {
-    public class IndexModel : PageModel
+    public partial class IndexModel : PageModel
     {
         public Version? SiteVersion { get; private set; }
 
         public IndexModel(IConfiguration config)
         {
             SiteVersion = config.GetVersion();
-        }
-
-        public void OnGet()
-        {
-
         }
     }
 }
