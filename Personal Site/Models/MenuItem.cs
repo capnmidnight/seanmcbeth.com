8d7870ada0c1a961a87efe97c04f8a0c59a1e587
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-11-04 17:10:35 -0400

    mastodon links

--- Personal Site/Models/MenuItem.cs
+++ Personal Site/Models/MenuItem.cs
@@ -1,4 +1,4 @@
 ﻿namespace SeanMcBeth.Models
 {
-    public record MenuItem(string Text, string Label, string Target, bool Hide = false);
+    public record MenuItem(string Text, string Label, string Target, bool Hide = false, string? Rel = null);
 }
