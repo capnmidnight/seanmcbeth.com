ee93c6201b10fd5e7ce77aae60cf137ae2699cf0
Sean T. McBeth <sean.mcbeth@gmail.com>
2022-12-22 21:12:25 -0500

    more tests

---
+++ TypeScript Code/src/pointer-app/index.ts
@@ -0,0 +1,57 @@
+import { touchAction } from "@juniper-lib/dom/css";
+import { onPointerCancel, onPointerDown, onPointerEnter, onPointerLeave, onPointerMove, onPointerOut, onTouchCancel, onTouchEnd, onTouchMove, onTouchStart } from "@juniper-lib/dom/evts";
+import { Div, elementApply, elementSetText } from "@juniper-lib/dom/tags";
+
+elementApply(document.body,
+    touchAction("none"),
+    onPointerCancel(handle),
+    onPointerDown(handle),
+    onPointerEnter(handle),
+    onPointerLeave(handle),
+    onPointerMove(handle),
+    onPointerOut(handle)
+);
+
+const touchStates = new Map<number, HTMLDivElement>();
+const found = new Set<number>();
+
+function handle(evt: PointerEvent) {
+    found.add(evt.pointerId);
+    if (!touchStates.has(evt.pointerId)) {
+        const tag = Div();
+        touchStates.set(evt.pointerId, tag);
+        elementApply("main", tag);
+    }
+
+    const tag = touchStates.get(evt.pointerId);
+    elementSetText(tag, `[${evt.type} ${evt.pointerType} ${evt.pointerId}] <${evt.clientX}, ${evt.clientY}>`);
+
+    const toDelete = new Set<number>();
+    for (const [key, value] of touchStates) {
+        if (!found.has(key)) {
+            toDelete.add(key);
+            value.remove();
+        }
+    }
+
+    for (const key of toDelete) {
+        touchStates.delete(key);
+    }
+}
+
+setInterval(() => {
+    const toDelete = new Set<number>();
+    for (const key of touchStates.keys()) {
+        if (!found.has(key)) {
+            toDelete.add(key);
+        }
+    }
+
+    found.clear();
+
+    for (const key of toDelete) {
+        const elem = touchStates.get(key);
+        elem.remove();
+        touchStates.delete(key);
+    }
+}, 20);
---
+++ TypeScript Code/src/touch-app/index.ts
@@ -0,0 +1,40 @@
+import { touchAction } from "@juniper-lib/dom/css";
+import { onTouchCancel, onTouchEnd, onTouchMove, onTouchStart } from "@juniper-lib/dom/evts";
+import { Div, elementApply, elementSetText } from "@juniper-lib/dom/tags";
+
+elementApply(document.body,
+    touchAction("none"),
+    onTouchStart(handle),
+    onTouchMove(handle),
+    onTouchCancel(handle),
+    onTouchEnd(handle)
+);
+
+const touchStates = new Map<number, HTMLDivElement>();
+
+function handle(evt: TouchEvent) {
+    const found = new Set<number>();
+    for (const touch of evt.touches) {
+        found.add(touch.identifier);
+        if (!touchStates.has(touch.identifier)) {
+            const tag = Div();
+            touchStates.set(touch.identifier, tag);
+            elementApply("main", tag);
+        }
+
+        const tag = touchStates.get(touch.identifier);
+        elementSetText(tag, `[${evt.type} ${touch.identifier}] <${touch.clientX}, ${touch.clientY}>`);
+    }
+
+    const toDelete = new Set<number>();
+    for (const [key, value] of touchStates) {
+        if (!found.has(key)) {
+            toDelete.add(key);
+            value.remove();
+        }
+    }
+
+    for (const key of toDelete) {
+        touchStates.delete(key);
+    }
+}
