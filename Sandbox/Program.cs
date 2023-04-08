984c9e9b2b15242dca134f5a478b78bfd0afc035
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-04-08 00:55:04 -0400

    resume update

---
+++ Sandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿using Juniper.Processes;
+
+var keyFile = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile))
+    .CD(".ssh")
+    .Touch("sean-mcbeth.pem");
+var cmd = new SSHCommand(
+    "Echo test", 
+    keyFile, 
+    "smcbeth", 
+    "seanmcbeth.com", 
+    "echo \"why does hello not work\"")
+    .InSubShell();
+
+cmd.Info += (sender, e) => Console.WriteLine("Info: {0}", e.Value);
+cmd.Warning += (sender, e) => Console.WriteLine("Wrn: {0}", e.Value);
+cmd.Err += (sender, e) => Console.WriteLine("Err: {0}", e.Value);
+
+
+await cmd.RunSafeAsync();
