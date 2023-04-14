ca781c99396502a49e883d56060f7b98020c7dd2
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-04-14 11:49:02 -0400

    cleanup

--- Marxent Labs Test/index.ts
+++ Marxent Labs Test/index.ts
@@ -11,46 +11,33 @@ import "./index.css";
 
 try {
     const canvas = document.querySelector<HTMLCanvasElement>("#frontBuffer");
-    // Creates a basic Babylon Scene object
-    const engine = new Engine(canvas, true, {
-        adaptToDeviceRatio: true,
-        antialias: true,
-    }, true);
-
-    const resizer = new ResizeObserver(() =>
-        engine.resize());
-
-    resizer.observe(canvas);
+    const engine = new Engine(canvas, true, { adaptToDeviceRatio: true });
     const scene = new Scene(engine);
 
-    const camera = new UniversalCamera("camera1",
-        new Vector3(0, 5, -10),
-        scene);
+    const resizer = new ResizeObserver(() => engine.resize());
+    resizer.observe(canvas);
 
+    const camera = new UniversalCamera("camera",
+        new Vector3(0, 5, -10));
     camera.setTarget(Vector3.Zero());
-    // Attaches the camera to the canvas
     camera.attachControl(canvas, true);
 
-    // Creates a light, aiming 0,1,0
     const light = new HemisphericLight("light",
         new Vector3(0, 1, 0),
         scene);
-    // Dim the light a small amount 0 - 1
     light.intensity = 0.7;
 
     const sphere = MeshBuilder.CreateSphere("sphere",
         { diameter: 2, segments: 32 },
         scene);
-    // Move sphere upward 1/2 its height
     sphere.position.y = 1;
 
     const ground = MeshBuilder.CreateGround("ground",
         { width: 6, height: 6 },
         scene);
 
+    engine.runRenderLoop(() => scene.render());
 
-    engine.runRenderLoop(() =>
-        scene.render());
 }
 catch (exp) {
     console.error(exp);
