dfa041c9af32a85405a369772f2935bdae42e598
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-05-06 08:50:43 -0400

    drawing calendars for print

---
+++ TypeScript Code/src/tests/calendar/index.ts
@@ -0,0 +1,91 @@
+﻿import { InputText, Span, TBody, TD, TH, THead, TR, Table, elementApply, elementClearChildren, elementGetText, elementSetDisplay } from "@juniper-lib/dom/tags";
+import { colSpan } from "@juniper-lib/dom/attrs";
+import { display } from "@juniper-lib/dom/css";
+import { onBlur, onEnterKeyPressed, onFocus, onMouseOut, onMouseOver } from "@juniper-lib/dom/evts";
+
+import "./index.css";
+
+function* gen<T>(n: number, action: (i: number) => T): IterableIterator<T> {
+    for (let i = 0; i < n; ++i) {
+        yield action(i);
+    }
+}
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const today = new Date();
+const index = ((today.getDate()) % 7) - today.getDay();
+const end = new Date(today.getFullYear(), today.getMonth() + 1, 0, 23, 59, 59, 999);
+const rows = Math.floor((end.getDate() + 1 + Math.abs(index)) / 7) + 1;
+
+
+function label(x: number, y: number): string {
+    const i = y * 7 + x + index;
+    const date = new Date(today.getFullYear(), today.getMonth(), i, 0, 0, 0, 0);
+    if (date.getMonth() === today.getMonth()) {
+        return date.getDate().toFixed(0);
+    }
+    else {
+        return "";
+    }
+}
+
+const title = Span(
+    months[today.getMonth()],
+    onMouseOver(() => {
+        entry.value = elementGetText(title);
+        elementSetDisplay(entry, true);
+        elementSetDisplay(title, false);
+    })
+);
+
+const entry = InputText(
+    display("none"),
+    onEnterKeyPressed(() => entry.blur()),
+    onFocus(() => {
+        entry.selectionStart = 0;
+        entry.selectionEnd = entry.value.length;
+    }),
+    onBlur(() => {
+        elementClearChildren(title);
+        elementApply(title, entry.value);
+        elementSetDisplay(entry, false);
+        elementSetDisplay(title, true);
+    }),
+    onMouseOut(() => {
+        if (document.activeElement !== entry) {
+            elementSetDisplay(entry, false);
+            elementSetDisplay(title, true);        
+        }
+    })
+);
+
+elementApply("main", Table(
+    THead(
+        TR(TH(colSpan(7), title, entry)),
+        TR(
+            ...gen(7, i => TH(days[i]))
+        )
+    ),
+    TBody(
+        ...gen(rows, y => TR(
+            ...gen(7, x => TD(
+                label(x, y)
+            ))
+        ))
+    )
+));
