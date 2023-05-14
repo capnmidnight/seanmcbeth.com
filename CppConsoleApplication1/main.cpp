435880861814f8876020a566ec79913f9a772063
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-05-14 19:08:50 -0400

    more fiddling

--- CppConsoleApplication1/main.cpp
+++ CppConsoleApplication1/main.cpp
@@ -6,17 +6,18 @@ using namespace stm;
 
 int main()
 {
+    atexit([] {
+        cout << "All done!" << endl;
+    });
+
     Calculator c;
     double x, y, result;
     char op;
 
     cout << "Calculator!!!" << endl;
-    while (true)
-    {
-        cin >> x >> op >> y;
-        result = c.Calculate(x, op, y);
-        cout << "Result: " << x << op << y << " = " << result << endl;
-    }
+    cin >> x >> op >> y;
+    result = c.Calculate(x, op, y);
+    cout << "Result: " << x << op << y << " = " << result << endl;
 
     return 0;
 }
