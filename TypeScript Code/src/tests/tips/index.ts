27a54a892d4bf5003160ced63136e3d1709ef3cc
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-08-17 21:32:16 -0400

    layout tweaks

--- TypeScript Code/src/tests/tips/index.ts
+++ TypeScript Code/src/tests/tips/index.ts
@@ -1,8 +1,8 @@
 import { Query } from "@juniper-lib/dom/attrs";
-import { Div } from "@juniper-lib/dom/tags";
+import { Main } from "@juniper-lib/dom/tags";
 import { TipBox } from "@juniper-lib/widgets/TipBox";
 
-Div(
+Main(
     Query("main"),
     TipBox("boxB",
         "Don't take any wooden nickels",
