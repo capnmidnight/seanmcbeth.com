3981dd584a6049278467b940752f2f5690e5051d
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-09-02 13:02:25 -0400

    cleanup config

--- seanmcbeth.com/Program.cs
+++ seanmcbeth.com/Program.cs
@@ -1,40 +1,16 @@
 using Juniper.Azure;
+using Juniper.OpenAI;
 using Juniper.Services;
 
-using OpenAI.Extensions;
+var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication
-    .CreateBuilder(args)
-    .ConfigureBuildSystem<SeanMcBeth.BuildConfig>()
-    .ConfigureJuniperWebApplication();
-
-var http = new HttpClient(new HttpClientHandler
-{
-    AllowAutoRedirect = false,
-    UseCookies = false
-});
-
-builder.Services.AddSingleton(http);
-
-var azureSubscriptionKey = builder.Configuration.GetValue<string>("Azure:Speech:SubscriptionKey");
-var azureRegion = builder.Configuration.GetValue<string>("Azure:Speech:Region");
-if (azureSubscriptionKey is not null
-    && azureRegion is not null)
-{
-    builder.Services.AddTransient<ISpeechService>(provider =>
-        new SpeechService(azureSubscriptionKey, azureRegion));
-}
-
-var openAIKey = builder.Configuration.GetValue<string>("OpenAI:APIKey");
-if (openAIKey is not null)
-{
-    builder.Services.AddOpenAIService(settings =>
-        settings.ApiKey = openAIKey);
-}
-
-var app = builder
+await builder
+    .UseSystemd()
+    .ConfigureJuniperBuildSystem<SeanMcBeth.BuildConfig>()
+    .ConfigureJuniperWebApplication()
+    .ConfigureJuniperHTTPClient()
+    .ConfigureJuniperSpeechService(builder.Configuration.GetSection("Azure:Speech"))
+    .ConfigureJuniperOpenAI(builder.Configuration.GetSection("OpenAI"))
     .Build()
-    .ConfigureJuniperRequestPipeline();
-
-await app.BuildReady();
-await app.RunAsync();
+    .ConfigureJuniperRequestPipeline()
+    .RunAfterBuildAsync();
