9a5ba04146e5270c9e48aa8f66ce6d7c0c18bb58
Sean T. McBeth <sean.mcbeth@gmail.com>
2023-09-04 22:57:45 -0400

    some publish settings

--- ImageViewer/Pages/Index.cshtml.cs
+++ ImageViewer/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Juniper.AppShell;
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ImageViewer.Pages
@@ -6,15 +8,17 @@ namespace ImageViewer.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IAppShell appShell;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IAppShell appShell)
         {
             _logger = logger;
+            this.appShell = appShell;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            await appShell.SetTitleAsync("Image Viewer - Home");
         }
     }
 }
