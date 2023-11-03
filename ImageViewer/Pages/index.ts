e40fc402e98e83fecb309edf8b6086b1d31e1670
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2023-11-02 21:02:56 -0400

    show JS console in AppShell.GTK

--- ImageViewer/Pages/index.ts
+++ ImageViewer/Pages/index.ts
@@ -1,2 +1,13 @@
+import { Button, Div } from "@juniper-lib/dom/src/tags";
 import "./index.css";
-console.log("Hello, world2");
+import { ID } from "@juniper-lib/dom/src/attrs";
+import { onClick } from "@juniper-lib/dom/src/evts";
+
+let counter = 0;
+Button(
+    ID("go"),
+    onClick(() => {
+        console.log("GO", ++counter);
+        document.body.append(Div(counter.toFixed(0)));
+    })
+);
--- ImageViewer/wwwroot/js/Pages/index.js
+++ ImageViewer/wwwroot/js/Pages/index.js
@@ -1,3 +1,425 @@
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/tslib/dist/typeChecks.js
+function t(o, s, c) {
+  return typeof o === s || o instanceof c;
+}
+function isFunction(obj) {
+  return t(obj, "function", Function);
+}
+function isString(obj) {
+  return t(obj, "string", String);
+}
+function isObject(obj) {
+  return isDefined(obj) && t(obj, "object", Object);
+}
+function isNullOrUndefined(obj) {
+  return obj === null || obj === void 0;
+}
+function isDefined(obj) {
+  return !isNullOrUndefined(obj);
+}
+
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/collections/dist/arrays.js
+function arrayClear(arr) {
+  return arr.splice(0);
+}
+function arrayRemove(arr, value) {
+  const idx = arr.indexOf(value);
+  if (idx > -1) {
+    arrayRemoveAt(arr, idx);
+    return true;
+  }
+  return false;
+}
+function arrayRemoveAt(arr, idx) {
+  return arr.splice(idx, 1)[0];
+}
+
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/events/dist/Task.js
+var Task = class {
+  /**
+   * Create a new Task
+   *
+   * @param autoStart - set to false to require manually starting the Task. Useful
+   * for reusable tasks that run on timers.
+   */
+  constructor(autoStart = true) {
+    this.autoStart = autoStart;
+    this.onThens = new Array();
+    this.onCatches = new Array();
+    this._result = void 0;
+    this._error = void 0;
+    this._executionState = "waiting";
+    this._resultState = "none";
+    this.resolve = (value) => {
+      if (this.running) {
+        this._result = value;
+        this._resultState = "resolved";
+        for (const thenner of this.onThens) {
+          thenner(value);
+        }
+        this.clear();
+        this._executionState = "finished";
+      }
+    };
+    this.reject = (reason) => {
+      if (this.running) {
+        this._error = reason;
+        this._resultState = "errored";
+        for (const catcher of this.onCatches) {
+          catcher(reason);
+        }
+        this.clear();
+        this._executionState = "finished";
+      }
+    };
+    if (this.autoStart) {
+      this.start();
+    }
+  }
+  clear() {
+    arrayClear(this.onThens);
+    arrayClear(this.onCatches);
+  }
+  /**
+   * If the task was not auto-started, signal that the task is now ready to recieve
+   * resolutions or rejections.
+   **/
+  start() {
+    this._executionState = "running";
+  }
+  /**
+   * Creates a resolving callback for a static value.
+   * @param value
+   */
+  resolver(value) {
+    return () => this.resolve(value);
+  }
+  resolveOn(target, resolveEvt, value) {
+    const resolver = this.resolver(value);
+    target.addEventListener(resolveEvt, resolver);
+    this.finally(() => target.removeEventListener(resolveEvt, resolver));
+  }
+  /**
+   * Get the last result that the task had resolved to, if any is available.
+   *
+   * If the Task had been rejected, attempting to get the result will rethrow
+   * the error that had rejected the task.
+   **/
+  get result() {
+    if (isDefined(this.error)) {
+      throw this.error;
+    }
+    return this._result;
+  }
+  /**
+   * Get the last error that the task had been rejected by, if any.
+   **/
+  get error() {
+    return this._error;
+  }
+  /**
+   * Get the current state of the task.
+   **/
+  get executionState() {
+    return this._executionState;
+  }
+  /**
+   * Returns true when the Task is hasn't started yet.
+   **/
+  get waiting() {
+    return this.executionState === "waiting";
+  }
+  /**
+   * Returns true when the Task is waiting to be resolved or rejected.
+   **/
+  get started() {
+    return this.executionState !== "waiting";
+  }
+  /**
+   * Returns true after the Task has started, but before it has finished.
+   **/
+  get running() {
+    return this.executionState === "running";
+  }
+  /**
+   * Returns true when the Task has been resolved or rejected.
+   **/
+  get finished() {
+    return this.executionState === "finished";
+  }
+  get resultState() {
+    return this._resultState;
+  }
+  /**
+   * Returns true if the Task had been resolved successfully.
+   **/
+  get resolved() {
+    return this.resultState === "resolved";
+  }
+  /**
+   * Returns true if the Task had been rejected, regardless of any
+   * reason being given.
+   **/
+  get errored() {
+    return this.resultState === "errored";
+  }
+  get [Symbol.toStringTag]() {
+    return this.toString();
+  }
+  /**
+   * Calling Task.then(), Task.catch(), or Task.finally() creates a new Promise.
+   * This method creates that promise and links it with the task.
+   **/
+  project() {
+    return new Promise((resolve, reject) => {
+      if (!this.finished) {
+        this.onThens.push(resolve);
+        this.onCatches.push(reject);
+      } else if (this.errored) {
+        reject(this.error);
+      } else {
+        resolve(this.result);
+      }
+    });
+  }
+  /**
+   * Attach a handler to the task that fires when the task is resolved.
+   *
+   * @param onfulfilled
+   * @param onrejected
+   */
+  then(onfulfilled, onrejected) {
+    return this.project().then(onfulfilled, onrejected);
+  }
+  /**
+   * Attach a handler that fires when the Task is rejected.
+   *
+   * @param onrejected
+   */
+  catch(onrejected) {
+    return this.project().catch(onrejected);
+  }
+  /**
+   * Attach a handler that fires regardless of whether the Task is resolved
+   * or rejected.
+   *
+   * @param onfinally
+   */
+  finally(onfinally) {
+    return this.project().finally(onfinally);
+  }
+  /**
+   * Resets the Task to an unsignalled state, which is useful for
+   * reducing GC pressure when working with lots of tasks.
+   **/
+  reset() {
+    this._reset(this.autoStart);
+  }
+  restart() {
+    this._reset(true);
+  }
+  _reset(start) {
+    if (this.running) {
+      this.reject("Resetting previous invocation");
+    }
+    this.clear();
+    this._result = void 0;
+    this._error = void 0;
+    this._executionState = "waiting";
+    this._resultState = "none";
+    if (start) {
+      this.start();
+    }
+  }
+};
+
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/dom/src/attrs.ts
+var warnings = /* @__PURE__ */ new Map();
+var HtmlAttr = class {
+  /**
+   * Creates a new setter functor for HTML Attributes
+   * @param key - the attribute name.
+   * @param value - the value to set for the attribute.
+   * @param bySetAttribute - whether the attribute should be set via the setAttribute method.
+   * @param tags - the HTML tags that support this attribute.
+   */
+  constructor(key, value, bySetAttribute, ...tags) {
+    this.key = key;
+    this.value = value;
+    this.bySetAttribute = bySetAttribute;
+    this.tags = tags.map((t2) => t2.toLocaleUpperCase());
+    Object.freeze(this);
+  }
+  tags;
+  /**
+   * Set the attribute value on an HTMLElement
+   * @param elem - the element on which to set the attribute.
+   */
+  applyToElement(elem) {
+    if (this.tags.length > 0 && this.tags.indexOf(elem.tagName) === -1) {
+      let set = warnings.get(elem.tagName);
+      if (!set) {
+        warnings.set(elem.tagName, set = /* @__PURE__ */ new Set());
+      }
+      if (!set.has(this.key)) {
+        set.add(this.key);
+        console.warn(`Element ${elem.tagName} does not support Attribute ${this.key}`);
+      }
+    }
+    if (this.bySetAttribute) {
+      elem.setAttribute(this.key, this.value.toString());
+    } else if (this.key in elem) {
+      elem[this.key] = this.value;
+    } else if (this.value === false) {
+      elem.removeAttribute(this.key);
+    } else if (this.value === true) {
+      elem.setAttribute(this.key, "");
+    } else if (isFunction(this.value)) {
+      this.value(elem);
+    } else {
+      elem.setAttribute(this.key, this.value.toString());
+    }
+  }
+};
+function attr(key, value, bySetAttribute, ...tags) {
+  return new HtmlAttr(key, value, bySetAttribute, ...tags);
+}
+function isAttr(obj) {
+  return obj instanceof HtmlAttr;
+}
+function ID(value) {
+  return attr("id", value, false);
+}
+function Type(value) {
+  if (!isString(value)) {
+    value = value.value;
+  }
+  return attr("type", value, false, "button", "input", "command", "embed", "link", "object", "script", "source", "style", "menu");
+}
+
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/dom/src/tags.ts
+function isErsatzElement(obj) {
+  if (!isObject(obj)) {
+    return false;
+  }
+  const elem = obj;
+  return elem.element instanceof Element;
+}
+function resolveElement(elem) {
+  if (isErsatzElement(elem)) {
+    return elem.element;
+  } else if (isString(elem)) {
+    return getElement(elem);
+  }
+  return elem;
+}
+function isIElementAppliable(obj) {
+  return isObject(obj) && "applyToElement" in obj && isFunction(obj.applyToElement);
+}
+function HtmlRender(element, ...children) {
+  const elem = element instanceof Element ? element : element instanceof ShadowRoot ? element : isString(element) ? document.querySelector(element) : element.element;
+  const target = elem instanceof HTMLTemplateElement ? elem.content : elem;
+  for (const child of children) {
+    if (isDefined(child)) {
+      if (child instanceof Node) {
+        target.appendChild(child);
+      } else if (isErsatzElement(child)) {
+        target.appendChild(resolveElement(child));
+      } else if (isIElementAppliable(child)) {
+        if (!(elem instanceof ShadowRoot)) {
+          child.applyToElement(elem);
+        }
+      } else {
+        target.appendChild(document.createTextNode(child.toLocaleString()));
+      }
+    }
+  }
+  return elem;
+}
+function getElement(selector) {
+  return document.querySelector(selector);
+}
+function HtmlTag(name, ...rest) {
+  let elem = null;
+  const finders = rest.filter(isAttr).filter((v) => v.key === "id" || v.key === "query");
+  for (const finder of finders) {
+    if (finder.key === "query") {
+      elem = finder.value;
+      arrayRemove(rest, finder);
+    } else if (finder.key === "id") {
+      elem = document.getElementById(finder.value);
+      if (elem) {
+        arrayRemove(rest, finder);
+      }
+    }
+  }
+  if (elem && elem.tagName !== name.toUpperCase()) {
+    console.warn(`Expected a "${name.toUpperCase()}" element but found a "${elem.tagName}".`);
+  }
+  if (!elem) {
+    elem = document.createElement(name);
+  }
+  HtmlRender(elem, ...rest);
+  return elem;
+}
+function ButtonRaw(...rest) {
+  return HtmlTag("button", ...rest);
+}
+function Button(...rest) {
+  return ButtonRaw(...rest, Type("button"));
+}
+function Div(...rest) {
+  return HtmlTag("div", ...rest);
+}
+
+// ../Juniper/src/Juniper.TypeScript/@juniper-lib/dom/src/evts.ts
+var HtmlEvt = class {
+  /**
+   * Creates a new setter functor for an HTML element event.
+   * @param name - the name of the event to attach to.
+   * @param callback - the callback function to use with the event handler.
+   * @param opts - additional attach options.
+   */
+  constructor(name, callback, opts) {
+    this.name = name;
+    this.callback = callback;
+    if (!isFunction(callback)) {
+      throw new Error("A function instance is required for this parameter");
+    }
+    this.opts = opts;
+    Object.freeze(this);
+  }
+  opts;
+  applyToElement(elem) {
+    this.add(elem);
+  }
+  /**
+   * Add the encapsulate callback as an event listener to the give HTMLElement
+   */
+  add(elem) {
+    elem.addEventListener(this.name, this.callback, this.opts);
+  }
+  /**
+   * Remove the encapsulate callback as an event listener from the give HTMLElement
+   */
+  remove(elem) {
+    elem.removeEventListener(this.name, this.callback);
+  }
+};
+function onEvent(eventName, callback, opts) {
+  return new HtmlEvt(eventName, callback, opts);
+}
+function onClick(callback, opts) {
+  return onEvent("click", callback, opts);
+}
+
 // Pages/index.ts
-console.log("Hello, world2");
+var counter = 0;
+Button(
+  ID("go"),
+  onClick(() => {
+    console.log("GO", ++counter);
+    document.body.append(Div(counter.toFixed(0)));
+  })
+);
 //# sourceMappingURL=index.js.map
